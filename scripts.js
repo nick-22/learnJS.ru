5b627d5459ec8fd8d4e821dcc1f3ec3db569c1b3
nick <kuzminov.n.i@gmail.com>
2018-09-02 22:04:43 +0300

    Условные операторы: if, '?'

--- scripts.js
+++ scripts.js
@@ -1,1 +1,73 @@
-alert('lets go!');
+//Вызов prompt возвращает то, что ввёл посетитель – строку или специальное значение null, если ввод отменён.
+
+// var quest = prompt('Каково «официальное» название JavaScript?','')
+// if (quest == 'ECMAScript') {
+//     alert('Верно');
+// }
+// else {
+//     alert('Не знаете? «ECMAScript»!');
+// }
+
+// var number = prompt('Введите число','22')
+// if (number > 0) {
+//     alert('1');
+// }
+// else if (number < 0) {
+//     alert('-1');
+// }
+// else if (number == 0) {
+//     alert('0');
+// }
+// else {
+//     alert('Это не число');
+// }
+
+// var login = prompt('Кто пришел','22');
+//
+// if (login == 'Админ') {
+//     var pass = prompt('Пароль?','22');
+//     if (pass == '22') {
+//         alert('Добро пожаловать');
+//     }
+//     else if (pass == null) {
+//         alert('Вход отменен');
+//     }
+//     else {
+//         alert('Не верный пароль');
+//     }
+// }
+//
+// else if (login == null) {
+//     alert('Вход отменен');
+// }
+//
+// else {
+//     alert('Я вас не знаю');
+// }
+
+
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+//
+// var result = (a + b < 4) ? 'Текст 1' : 'текст 2'
+
+
+// var message;
+//
+// if (login == 'Вася') {
+//   message = 'Привет';
+// } else if (login == 'Директор') {
+//   message = 'Здравствуйте';
+// } else if (login == '') {
+//   message = 'Нет логина';
+// } else {
+//   message = '';
+// }
+//
+// message = (login == 'Вася') ? 'Привет' :
+// (login == 'Директор') ? 'Здравствуйте' :
+// (login == '') ? 'Нет логина' :
+// '';
