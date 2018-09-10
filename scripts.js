a9d521dfdd4a7f329c65634d77ba7aabc04187fd
nick <kuzminov.n.i@gmail.com>
2018-09-10 22:52:35 +0300

    Функции. Тестировал иеорию

--- scripts.js
+++ scripts.js
@@ -1,59 +1,98 @@
-// switch (browser) {
-//   case 'IE':
-//     alert( 'О, да у вас IE!' );
-//     break;
+// 1
+// var a =2;
+// var b = 5;
+// var c=4;
+// var x = 0;
+// function calcD() {
+//     // a = 100;
+//     x = b*b - 4*a*c;
+//     alert(x);
+// }
 //
-//   case 'Chrome':
-//   case 'Firefox':
-//   case 'Safari':
-//   case 'Opera':
-//     alert( 'Да, и эти браузеры мы поддерживаем' );
-//     break;
+// calcD();
 //
-//   default:
-//     alert( 'Мы надеемся, что и в вашем браузере все ок!' );
+// alert(x);
+// alert(a);
+
+// 2
+
+// function calcD() {
+//     a = 2;
+//     b = 3;
+//     c = 4;
+//     alert(b*b-4*a*c);
+// }
+//
+// calcD();
+
+// 3
+
+// function calcD(a, b, c) {
+//     var x = b*b-4*a*c;
+//     alert(x+=1);
+// }
+//
+// calcD(2, 3, 4);
+
+// 4
+
+// function calcD(a, b, c) {
+//     alert(b*b-4*a*c);
+// }
+//
+// calcD(2, 3, 4);
+
+// 5
+
+// function calcD(a, b, c) {
+//     return b*b-4*a*c;
+// }
+//
+// var test = calcD(2, 3, 4);
+// alert(test+=1);
+
+// 6
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   }
+//   else if (age < 3) { //позже добавил выход из функции
+//       return;
+//   }
+//   else {
+//     return confirm('Родители разрешили?');
+//   }
+// }
+//
+// var age = prompt('Ваш возраст?');
+//
+// if (checkAge(age)) {
+//   alert( 'Доступ разрешен' );
+// }
+// // else if (checkAge(age) === undefined ) { //позже добавил, чтобы заценить пустой return
+// //     alert( 'Проверка на undefined' );
 // // }
-
-// if (browser == 'IE') {
-//     alert( 'О, да у вас IE!' );
-// }
-// else if (browser == 'Chrome'
-// || browser == 'Firefox'
-// || browser == 'Safari'
-// || browser == 'Opera') {
-//     alert( 'Да, и эти браузеры мы поддерживаем' );
-// }
-// else alert( 'Мы надеемся, что и в вашем браузере все ок!' );
-
-// var a = +prompt('a?', '');
-//
-// if (a == 0) {
-//   alert( 0 );
-// }
-// if (a == 1) {
-//   alert( 1 );
-// }
-//
-// if (a == 2 || a == 3) {
-//   alert( '2,3' );
+// else {
+//   alert( 'В доступе отказано' );
 // }
 
-// var a = +prompt('a?', '');
+// 7 то же, что и 6, но через if/else
+
+// var age = prompt('Ваш возраст?');
 //
-// switch (a) {
-//     case 0:
-//         alert(0);
-//         break;
-//
-//     case 1:
-//         alert(1);
-//         break;
-//
-//     case 2:
-//     case 3:
-//         alert( '2,3' );
-//         break;
-//
-//     default:
-//         alert('other');
+// if (age > 18) {
+//     alert('Доступ разрешен');
+// }
+// else if(!age) {
+//     alert('Ввод отменен');
+// }
+// else {
+//     var allow = confirm('Родители разрешили?');
+//     if (allow) {
+//         alert('Доступ разрешен');
+//     }
+//     else {
+//         alert( 'В доступе отказано' );
+//     }
 // }
